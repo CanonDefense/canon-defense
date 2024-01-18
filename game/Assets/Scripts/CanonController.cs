0dbb3bedadda56f6b3186494533f97511abc6abc
Kevin Cerro DVRV <kevincerro.dvrv@gmail.com>
2024-01-18 21:56:25 +0100

    Allow to fire canon

--- game/Assets/Scripts/CanonController.cs
+++ game/Assets/Scripts/CanonController.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float rotationSpeed = 5f;
-    private readonly float rotationOffsetDegrees = -90;
+    public float bulletSpeed = 10f;
+    public Transform firePoint;
+    private readonly float rotationOffsetDegrees = -90f;
+    public GameObject bulletPrefab;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +30,21 @@ public class PlayerController : MonoBehaviour
 
     private void MoveCanon()
     {
+        transform.rotation = GetRotationTowardsMouse(rotationOffsetDegrees);
+    }
+
+    private void FireCanon()
+    {
+        Vector3 bulletDirection = GetRotationTowardsMouse() * Vector3.right;
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().AddForce(bulletDirection * bulletSpeed, ForceMode2D.Impulse);
+
+        Destroy(bullet, 10);
+    }
+
+    private Quaternion GetRotationTowardsMouse(float offset = 0f)
+    {
         // Get mouse position in world coordinates
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -33,12 +52,8 @@ public class PlayerController : MonoBehaviour
         float angle = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
 
         // Rotate the object towards the mouse
-        Quaternion rotation = Quaternion.Euler(0f, 0f, angle + rotationOffsetDegrees);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-    }
+        Quaternion rotation = Quaternion.Euler(0f, 0f, angle + offset);
 
-    private void FireCanon()
-    {
-        Debug.Log("Fire");
+        return rotation;
     }
 }
